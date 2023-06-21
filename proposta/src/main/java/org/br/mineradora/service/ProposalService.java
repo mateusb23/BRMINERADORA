dbd5891a2c623e983df84ce2f5b8d60ad182e6b9
mateusb23 <mateusb.bispo@gmail.com>
2023-06-21 20:44:04 -0300

    update proposta service and creating report service

---
+++ proposta/src/main/java/org/br/mineradora/service/ProposalService.java
@@ -0,0 +1,13 @@
+package org.br.mineradora.service;
+
+import org.br.mineradora.dto.ProposalDetailsDTO;
+
+public interface ProposalService {
+
+    ProposalDetailsDTO findFullProposal(long id);
+
+    void createNewProposal(ProposalDetailsDTO proposalDetailsDTO);
+
+    void removeProposal(long id);
+
+}
---
+++ proposta/src/main/java/org/br/mineradora/service/ProposalServiceImpl.java
@@ -0,0 +1,71 @@
+package org.br.mineradora.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import org.br.mineradora.dto.ProposalDTO;
+import org.br.mineradora.dto.ProposalDetailsDTO;
+import org.br.mineradora.entity.ProposalEntity;
+import org.br.mineradora.message.KafkaEvent;
+import org.br.mineradora.repository.ProposalRepository;
+
+import java.util.Date;
+
+@ApplicationScoped
+public class ProposalServiceImpl implements ProposalService {
+
+    @Inject
+    ProposalRepository proposalRepository;
+
+    @Inject
+    KafkaEvent kafkaEvent;
+
+    @Override
+    public ProposalDetailsDTO findFullProposal(long id) {
+        ProposalEntity proposal = proposalRepository.findById(id);
+        return ProposalDetailsDTO.builder()
+                .proposalId(proposal.getId())
+                .proposalValidityDays(proposal.getProposalValidityDays())
+                .country(proposal.getCountry())
+                .priceTonne(proposal.getPriceTonne())
+                .customer(proposal.getCustomer())
+                .tonnes(proposal.getTonnes())
+                .build();
+    }
+
+    @Override
+    @Transactional
+    public void createNewProposal(ProposalDetailsDTO proposalDetailsDTO) {
+        ProposalDTO proposal = buildAndSaveNewProposal(proposalDetailsDTO);
+        kafkaEvent.sendNewKafkaEvent(proposal);
+    }
+
+    @Override
+    @Transactional
+    public void removeProposal(long id) {
+        proposalRepository.deleteById(id);
+    }
+
+    @Transactional
+    public ProposalDTO buildAndSaveNewProposal(ProposalDetailsDTO proposalDetailsDTO) {
+        try {
+            ProposalEntity proposal = new ProposalEntity();
+            proposal.setCreated(new Date());
+            proposal.setProposalValidityDays(proposalDetailsDTO.getProposalValidityDays());
+            proposal.setCountry(proposalDetailsDTO.getCountry());
+            proposal.setCustomer(proposalDetailsDTO.getCustomer());
+            proposal.setPriceTonne(proposalDetailsDTO.getPriceTonne());
+            proposal.setTonnes(proposalDetailsDTO.getTonnes());
+            proposalRepository.persist(proposal);
+            return ProposalDTO.builder()
+                    .proposalId(proposalRepository.findByCustomer(proposal.getCustomer()).get().getId())
+                    .priceTonne(proposal.getPriceTonne())
+                    .customer(proposal.getCustomer())
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+}
